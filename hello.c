7dab5d4d323da2270c2ca01c1b87c1e75828f514
mwaidoo <michaelwireduaidoo@gmail.com>
2024-06-25 13:12:19 -0400

    Update hello.c (add '\n')

--- hello.c
+++ hello.c
@@ -1,11 +1,11 @@
 // Name		: hello.c
 // Author	: Michael Aidoo
 // Created	: 06-13-2024
-// Description	: writes the file hello.txt with contents "Hello World!"
+// Description	: writes the file hello.txt with contents "Hello World!\n"
 
 #include <stdio.h>
 
 int main() {
-	printf("Hello World!");
+	printf("Hello World!\n");
 	return 0;
 }
